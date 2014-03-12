ca24894f598e3a47c30363570190e3ad89889219
Da_Blitz <code@pocketnix.org>
2014-03-12 23:25:29 +1100

    Add vmsplice support and example

--- butter/splice.py
+++ butter/splice.py
@@ -15,12 +15,34 @@ _ffi.cdef("""
 #define SPLICE_F_MORE     ... /* After splice() more data will be sent, this is a hint to add TCP_CORK like buffering */
 #define SPLICE_F_GIFT     ... /* unused for splice() (vmsplice compatibility) */
 
+#define IOV_MAX ... /* Maximum ammount of vectors that can be written by vmsplice in one go */
+
+struct iovec {
+    void *iov_base; /* Starting address */
+    size_t iov_len; /* Number of bytes */
+};
+
 ssize_t splice(int fd_in, signed long long *off_in, int fd_out, signed long long *off_out, size_t len, unsigned int flags);
 ssize_t tee(int fd_in, int fd_out, size_t len, unsigned int flags);
+ssize_t vmsplice(int fd, const struct iovec *iov, unsigned long nr_segs, unsigned int flags);
+
+char * convert_str_to_void(char * buf);
 """)
 
 _C = _ffi.verify("""
+#include <limits.h> /* used to define IOV_MAX */
 #include <fcntl.h>
+#include <sys/uio.h>
+
+/* Its really hard in cffi to convert a python string to a char * WITHOUT using a function
+   so instead lets just make a dummy function and use that. while we are at it, lets do
+   the conversion to a void pointer as well so we dont need to much with types
+*/
+void * convert_str_to_void(char * buf){
+    /* Take a string and convert it over to a void pointer */
+    /* While simple, this is a work around for the cffi lib in python */
+    return (void *)buf;
+};
 """, libraries=[])
 
 def splice(fd_in, fd_out, in_offset=0, out_offset=0, len=0, flags=0):
@@ -134,11 +156,65 @@ def tee(fd_in, fd_out, len=0, flags=0):
     return size
 
 
+def vmsplice(fd, vec, flags=0):
+    """Write a list strings or byte buffers to the specified fd
+    
+    Arguments
+    ----------
+    :param file fd: File object or fd to write to
+    :param list vec: A list of strings to write to the pipe
+    :param int flags: Flags to specify extra options
+    
+    Flags
+    ------
+    SPLICE_F_MOVE: This is a noop in modern kernels and is left here for compatibility
+    SPLICE_F_NONBLOCK: Make vmsplice operations Non blocking (as long as the fd is non blocking)
+    SPLICE_F_MORE: unused for vmsplice()
+    SPLICE_F_GIFT: Pass ownership of the pages to the kernel. You must not modify data in place
+                   if using this option as the pages now belong to the kernel and bad things (tm)
+                   will happen
+                   if used, pages must be page alighned in both length and position
+    Returns
+    --------
+    :return: Number of bytes written
+    :rtype: int
+    
+    Exceptions
+    -----------
+    :raises ValueError: One of the file descriptors is not a pipe
+    :raises ValueError: Both file descriptors refer to the same pipe
+    :raises MemoryError: Insufficient kernel memory
+    """
+    fd = getattr(fd, 'fileno', lambda: fd)()
+    n_vec =_ffi.new('struct iovec[]', len(vec))
+    for str, v in zip(vec, n_vec) :
+        v.iov_base = _C.convert_str_to_void(str)
+        v.iov_len = len(str)
+    
+    size = _C.vmsplice(fd, n_vec, len(vec), flags)
+
+    if size < 0:
+        err = _ffi.errno
+        if err == _errno.EBADF:
+            raise ValueError("fd is not valid or does not refer to a pipe")
+        if err == _errno.EINVAL:
+            raise ValueError("nr_segs is 0 or greater than IOV_MAX; or memory not aligned if SPLICE_F_GIFT set")
+        elif err == _errno.ENOMEM:
+            raise MemoryError("Insufficent kernel memory avalible")
+        else:
+            # If you are here, its a bug. send us the traceback
+            raise ValueError("Unknown Error: {}".format(err))
+
+    return size
+
 
 SPLICE_F_MOVE = _C.SPLICE_F_MOVE    
 SPLICE_F_NONBLOCK = _C.SPLICE_F_NONBLOCK
 SPLICE_F_MORE = _C.SPLICE_F_MORE    
 SPLICE_F_GIFT = _C.SPLICE_F_GIFT    
+
+IOV_MAX = _C.IOV_MAX
+
 
 def main():
     """Simple test to confirm that splice works and can handle short reads
@@ -260,6 +336,21 @@ def tee_main():
         if command in sys.stdin.read(bytes):
             print("Exit command ({}) found, exiting".format(command))
             break
+
+def vmsplice_main():
+    import sys
+    import os
+
+    r_fd, w_fd = os.pipe()
+    
+    buf = ["this is a test",
+           " and this should be on the same line",
+           "\nsupprise, newline this time\n"
+          ]
+    bytes = vmsplice(w_fd, buf)
+    print('vmspliced {} bytes'.format(bytes))
+    splice(r_fd, sys.stdout, len=bytes)
+    print('spliced {} bytes'.format(bytes))
 
 if __name__ == "__main__":
     socket_main()
