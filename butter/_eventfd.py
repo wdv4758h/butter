a624f5fba675f2fb3ac45552a94addd875e2fa37
Da_Blitz <code@pocketnix.org>
2014-05-25 22:38:31 +1000

    Split eventfd up into 2 parts, low level and high level

---
+++ butter/_eventfd.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+"""eventfd: maintain an atomic counter inside a file descriptor"""
+from cffi import FFI
+import errno
+
+ffi = FFI()
+ffi.cdef("""
+#define EFD_CLOEXEC ...
+#define EFD_NONBLOCK ...
+#define EFD_SEMAPHORE ...
+
+int eventfd(unsigned int initval, int flags);
+""")
+
+C = ffi.verify("""
+#include <sys/eventfd.h>
+#include <stdint.h> /* Definition of uint64_t */
+""", libraries=[])
+
+def eventfd(inital_value=0, flags=0):
+    """Create a new eventfd
+    
+    Arguments
+    ----------
+    :param int inital_value: The inital value to set the eventfd to
+    :param int flags: Flags to specify extra options
+    
+    Flags
+    ------
+    EFD_CLOEXEC: Close the eventfd when executing a new program
+    EFD_NONBLOCK: Open the socket in non-blocking mode
+    EFD_SEMAPHORE: Provide semaphore like semantics for read operations
+    
+    Returns
+    --------
+    :return: The file descriptor representing the eventfd
+    :rtype: int
+    
+    Exceptions
+    -----------
+    :raises ValueError: Invalid value in flags
+    :raises OSError: Max per process FD limit reached
+    :raises OSError: Max system FD limit reached
+    :raises OSError: Could not mount (internal) anonymous inode device
+    :raises MemoryError: Insufficient kernel memory
+    """
+    fd = C.eventfd(inital_value, flags)
+    
+    if fd < 0:
+        err = ffi.errno
+        if err == errno.EINVAL:
+            raise ValueError("Invalid value in flags")
+        elif err == errno.EMFILE:
+            raise OSError("Max per process FD limit reached")
+        elif err == errno.ENFILE:
+            raise OSError("Max system FD limit reached")
+        elif err == errno.ENODEV:
+            raise OsError("Could not mount (internal) anonymous inode device")
+        elif err == errno.ENOMEM:
+            raise MemoryError("Insufficent kernel memory available")
+        else:
+            # If you are here, its a bug. send us the traceback
+            raise ValueError("Unknown Error: {}".format(err))
+
+    return fd
+
+def str_to_events(str):
+    value = ffi.new('uint64_t[1]')
+    ffi.buffer(value, 8)[0:8] = str
+
+    return [value[0]] # this may seem redundent but the original
+                      # container is not actually a list
+
+def event_to_str(event):
+    # We use ffi rather than the array module as
+    # python2.7 does not have an unsigned 64 bit in type
+    event = ffi.new('uint64_t[1]', (event,))
+    packed_event = ffi.buffer(event)[:]
+    
+    return packed_event
+
+EFD_CLOEXEC = C.EFD_CLOEXEC
+EFD_NONBLOCK = C.EFD_NONBLOCK
+EFD_SEMAPHORE = C.EFD_SEMAPHORE
--- butter/eventfd.py
+++ butter/eventfd.py
@@ -5,74 +5,10 @@ from __future__ import print_function
 
 from .utils import Eventlike as _Eventlike
 
-from os import write as _write, read as _read, close as _close
-from cffi import FFI as _FFI
-import errno as _errno
-
-_ffi = _FFI()
-_ffi.cdef("""
-#define EFD_CLOEXEC ...
-#define EFD_NONBLOCK ...
-#define EFD_SEMAPHORE ...
-
-int eventfd(unsigned int initval, int flags);
-""")
-
-_C = _ffi.verify("""
-#include <sys/eventfd.h>
-#include <stdint.h> /* Definition of uint64_t */
-""", libraries=[])
-
-def eventfd(inital_value=0, flags=0):
-    """Create a new eventfd
-    
-    Arguments
-    ----------
-    :param int inital_value: The inital value to set the eventfd to
-    :param int flags: Flags to specify extra options
-    
-    Flags
-    ------
-    EFD_CLOEXEC: Close the eventfd when executing a new program
-    EFD_NONBLOCK: Open the socket in non-blocking mode
-    EFD_SEMAPHORE: Provide semaphore like semantics for read operations
-    
-    Returns
-    --------
-    :return: The file descriptor representing the eventfd
-    :rtype: int
-    
-    Exceptions
-    -----------
-    :raises ValueError: Invalid value in flags
-    :raises OSError: Max per process FD limit reached
-    :raises OSError: Max system FD limit reached
-    :raises OSError: Could not mount (internal) anonymous inode device
-    :raises MemoryError: Insufficient kernel memory
-    """
-    fd = _C.eventfd(inital_value, flags)
-    
-    if fd < 0:
-        err = _ffi.errno
-        if err == _errno.EINVAL:
-            raise ValueError("Invalid value in flags")
-        elif err == _errno.EMFILE:
-            raise OSError("Max per process FD limit reached")
-        elif err == _errno.ENFILE:
-            raise OSError("Max system FD limit reached")
-        elif err == _errno.ENODEV:
-            raise OsError("Could not mount (internal) anonymous inode device")
-        elif err == _errno.ENOMEM:
-            raise MemoryError("Insufficent kernel memory available")
-        else:
-            # If you are here, its a bug. send us the traceback
-            raise ValueError("Unknown Error: {}".format(err))
-
-    return fd
-
-EFD_CLOEXEC = _C.EFD_CLOEXEC
-EFD_NONBLOCK = _C.EFD_NONBLOCK
-EFD_SEMAPHORE = _C.EFD_SEMAPHORE
+from ._eventfd import EFD_CLOEXEC, EFD_NONBLOCK, EFD_SEMAPHORE
+from ._eventfd import str_to_events, event_to_str
+from ._eventfd import eventfd
+import os as _os
 
 class Eventfd(_Eventlike):
     def __init__(self, inital_value=0, flags=0):
@@ -97,9 +33,8 @@ class Eventfd(_Eventlike):
         
         :param int value: The value to increment the counter by (default=1)
         """
-        packed_value = _ffi.new('uint64_t[1]', (value,))
-        packed_value = _ffi.buffer(packed_value)[:]
-        _write(self.fileno(), packed_value)
+        packed_value = event_to_str(value)
+        _os.write(self.fileno(), packed_value)
 
     def _read_events(self):
         """Read the current value of the counter and zero the counter
@@ -109,12 +44,11 @@ class Eventfd(_Eventlike):
         :return: The current count of the timer
         :rtype: int
         """
-        data = _read(self.fileno(), 8)
-        value = _ffi.new('uint64_t[1]')
-        _ffi.buffer(value, 8)[0:8] = data
+        data = _os.read(self.fileno(), 8)
+        events = str_to_events(data)
 
-        return [value[0]] # this may seem redundent but the original
-                          # container is not actually a list
+        return events
+
 
     def __int__(self):
         return self.read_event()
