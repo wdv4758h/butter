e686dc5ba97c31727c05b2a536165cfd9091c78b
Da_Blitz <code@pocketnix.org>
2015-03-10 01:34:39 +1100

    Bump verion number

--- butter/__init__.py
+++ butter/__init__.py
@@ -2,7 +2,7 @@
 """Butter: library to give python access to linux's more lower level features"""
 
 __author__ = "Da_Blitz"
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 __email__ = "code@pocketnix.org"
 __license__ = "BSD (3 Clause)"
 __url__ = "http://code.pocketnix.org/butter"
