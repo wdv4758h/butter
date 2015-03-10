49e51c2ecd77235c7d2e9fa5d368d43e88703507
Da_Blitz <code@pocketnix.org>
2015-03-10 23:02:20 +1100

    More paramertrised tests

--- tests/unit/test_execeptions.py
+++ tests/unit/test_execeptions.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 
 from butter import eventfd, _eventfd
+from butter import fanotify, _fanotify
+from butter.utils import PermissionError
 from pytest import raises
 import pytest
 import errno
 
-@pytest.mark.parametrize('path,module,func,errno,exception', [
-                         ('butter._eventfd.C.eventfd', _eventfd, _eventfd.eventfd, errno.EINVAL, ValueError),
-                         ('butter._eventfd.C.eventfd', _eventfd, _eventfd.eventfd, errno.EMFILE, OSError),
-                         ('butter._eventfd.C.eventfd', _eventfd, _eventfd.eventfd, errno.ENFILE, OSError), # errno is diffrent to above
-                         ('butter._eventfd.C.eventfd', _eventfd, _eventfd.eventfd, errno.ENODEV, OSError),
-                         ('butter._eventfd.C.eventfd', _eventfd, _eventfd.eventfd, errno.ENOMEM, MemoryError),
-                         ('butter._eventfd.C.eventfd', _eventfd, _eventfd.eventfd, errno.EHOSTDOWN, ValueError), # errno chosen as unused in our code
+@pytest.mark.parametrize('path,module,func,args,errno,exception', [
+                         ('butter._eventfd.C.eventfd', _eventfd, _eventfd.eventfd, (), errno.EINVAL, ValueError),
+                         ('butter._eventfd.C.eventfd', _eventfd, _eventfd.eventfd, (), errno.EMFILE, OSError),
+                         ('butter._eventfd.C.eventfd', _eventfd, _eventfd.eventfd, (), errno.ENFILE, OSError), # errno is diffrent to above
+                         ('butter._eventfd.C.eventfd', _eventfd, _eventfd.eventfd, (), errno.ENODEV, OSError),
+                         ('butter._eventfd.C.eventfd', _eventfd, _eventfd.eventfd, (), errno.ENOMEM, MemoryError),
+                         ('butter._eventfd.C.eventfd', _eventfd, _eventfd.eventfd, (), errno.EHOSTDOWN, ValueError), # errno chosen as unused in our code
+
+                         ('butter._fanotify.C.fanotify_init', _fanotify, _fanotify.fanotify_init, (0,), errno.EINVAL, ValueError),
+                         ('butter._fanotify.C.fanotify_init', _fanotify, _fanotify.fanotify_init, (0,), errno.EMFILE, OSError),
+                         ('butter._fanotify.C.fanotify_init', _fanotify, _fanotify.fanotify_init, (0,), errno.ENOMEM, MemoryError),
+                         ('butter._fanotify.C.fanotify_init', _fanotify, _fanotify.fanotify_init, (0,), errno.EPERM,  PermissionError),
+                         ('butter._fanotify.C.fanotify_init', _fanotify, _fanotify.fanotify_init, (0,), errno.EHOSTDOWN, ValueError), # errno chosen as unused in our code
                          ])
 @pytest.mark.unit
-def test_exception(mocker, path, module, func, errno, exception):
+def test_exception(mocker, path, module, func, args, errno, exception):
     # patch the underlying function as exposed by cffi
     m = mocker.patch(path)
     # -1 forces most of our code to check ffi.errno
@@ -25,4 +33,4 @@ def test_exception(mocker, path, module, func, errno, exception):
     
     # Call the same function as the user and wait for it to blow up
     with raises(exception):
-        func()
+        func(*args)
