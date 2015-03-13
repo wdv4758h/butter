81f59e6b87767f9d39edf0fa6d2a509755e9d966
Da_Blitz <code@pocketnix.org>
2015-03-14 02:43:12 +1100

    Clean up clone.py implementation

--- butter/clone.py
+++ butter/clone.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
+from .utils import PermissionError
 from cffi import FFI
+import errno
+
 ffi = FFI()
 ffi.cdef("""
 
@@ -12,8 +15,9 @@ ffi.cdef("""
 #define CLONE_NEWPID  ...
 #define CLONE_NEWNET  ...
 
-long __clone(unsigned long flags, void *child_stack, ...);
-//           void *ptid, void *ctid, struct pt_regs *regs);
+//#long __clone(unsigned long flags, void *child_stack, ...);
+long __clone(unsigned long flags, void *child_stack,
+             void *ptid, void *ctid, void *regs);
 
 int unshare(int flags);
 int setns(int fd, int nstype);
@@ -27,9 +31,10 @@ C = ffi.verify("""
 #include <unistd.h>
 
 // man page
-long __clone(unsigned long flags, void *child_stack, ...);
-//           void *ptid, void *ctid,
-//           struct pt_regs *regs);
+//long __clone(unsigned long flags, void *child_stack, ...);
+long __clone(unsigned long flags, void *child_stack,
+             void *ptid, void *ctid,
+             void *regs);
 """, libraries=[])
 
 CLONE_ALL = C.CLONE_NEWIPC  | \
@@ -39,7 +44,6 @@ CLONE_ALL = C.CLONE_NEWIPC  | \
             C.CLONE_NEWPID  | \
             C.CLONE_NEWUSER
 
-CLONE_FS = C.CLONE_FS
 CLONE_NEWNS = C.CLONE_NEWNS
 CLONE_NEWUTS = C.CLONE_NEWUTS
 CLONE_NEWIPC = C.CLONE_NEWIPC
@@ -47,18 +51,67 @@ CLONE_NEWUSER = C.CLONE_NEWUSER
 CLONE_NEWPID = C.CLONE_NEWPID
 CLONE_NEWNET = C.CLONE_NEWNET
 
+
+def unshare(flags):
+    """Unshare the current namespace and create a new one
+
+    Arguments
+    ----------
+    :param int flags: The flags controlling which namespaces to unshare
+
+    Flags
+    ------
+    CLONE_NEWNS: Unshare the mount namespace causing mounts in this namespace
+                 to not be visible to the parent namespace
+    CLONE_NEWUTS: Unshare the system hostname allowing it to be changed independently
+                  to the rest of the system
+    CLONE_NEWIPC: Unshare the IPC namespace 
+    CLONE_NEWUSER: Unshare the UID space allowing UIDs to be remapped to the parent
+    CLONE_NEWPID: Unshare the PID space allowing remapping of PIDs relative to the parent
+    CLONE_NEWNET: Unshare the network namespace, creating a separate set of network
+                  interfaces/firewall rules
+
+    Exceptions
+    -----------
+    :raises ValueError: Invalid value in flags
+    """
+    fd = C.unshare(flags)
+
+    if fd < 0:
+        err = ffi.errno
+        if err == errno.EINVAL:
+            raise ValueError("Invalid value in flags")
+        elif err == errno.EPERM:
+            raise PermissionError("Process in chroot or has incorrect permissions")
+        elif err == errno.EUSERS:
+            raise PermissionError("CLONE_NEWUSER specified but max user namespace nesting has been reached")
+        elif err == errno.ENOMEM:
+            raise MemoryError("Insufficent kernel memory available")
+        else:
+            # If you are here, its a bug. send us the traceback
+            raise ValueError("Unknown Error: {}".format(err))
+
+    return fd
+
+
 ### prctl
 
 def main():
-    tid = C.__clone(CLONE_NEWUTS|CLONE_NEWNET, 0,0,0,0)
+    import os, errno, sys
     
-    if tid == 0:
-        # child
-        os.execl('/bin/bash', ['bash'])
+#    ret = C.__clone(CLONE_NEWNET|CLONE_NEWUTS|CLONE_NEWIPC|CLONE_NEWNS, ffi.NULL)
+#    ret = C.__clone(CLONE_NEWNET|CLONE_NEWUTS|CLONE_NEWIPC|CLONE_NEWNS, ffi.NULL, ffi.NULL, ffi.NULL, ffi.NULL)
+
+    ret = C.unshare(CLONE_NEWNET|CLONE_NEWUTS|CLONE_NEWIPC|CLONE_NEWNS)
+#    ret = C.unshare(CLONE_ALL)
+    if ret >= 0:
+#        with open("/proc/self/uid_map", "w") as f:
+#            f.write("0 0 1\n")
+        os.execl('/bin/bash', 'bash')
     else:
-        # parent
-        os.wait()
-        print('child exited')
+        print(ret, ffi.errno, errno.errorcode[ffi.errno])
+        print("failed")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
