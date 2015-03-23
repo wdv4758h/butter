a23b108cc4c3a00642ceb052d654d91d5b755d9d
Da_Blitz <code@pocketnix.org>
2015-03-23 22:19:09 +1100

    Start testing TimerSpec and TimerVals repr()

--- tests/unit/test_repr.py
+++ tests/unit/test_repr.py
@@ -2,7 +2,7 @@ from butter.eventfd import Eventfd
 from butter.fanotify import Fanotify
 from butter.inotify import Inotify
 from butter.signalfd import Signalfd
-from butter.timerfd import Timerfd
+from butter.timerfd import Timerfd, TimerVal, TimerSpec
 import pytest
 
 
@@ -31,3 +31,34 @@ def test_repr_fd(obj):
 def test_repr_fd_closed(obj):
     obj._fd = None
     assert 'fd=closed' in repr(obj), "Instance does not indicate it is closed"
+
+@pytest.mark.repr
+@pytest.mark.unit
+def test_timerval():
+    t = TimerVal().offset(1, 2).repeats(3, 4)
+    r = repr(t)
+    assert t.__class__.__name__ in r, 'Does not contain its own name'
+    assert '1' in r, 'Value not in output'
+    assert '1s' in r, 'Value does not have units'
+    assert '2' in r, 'Value not in output'
+    assert '2ns' in r, 'Value does not have units'
+    assert '3' in r, 'Value not in output'
+    assert '3s' in r, 'Value does not have units'
+    assert '4' in r, 'Value not in output'
+    assert '4ns' in r, 'Value does not have units'
+
+@pytest.mark.repr
+@pytest.mark.unit
+def test_timerspec():
+    t = TimerSpec(one_off_seconds=1,
+                    one_off_nano_seconds=200000000,
+                    reoccuring_seconds=3,
+                    reoccuring_nano_seconds=400000000,
+                   )
+    r = repr(t)
+    assert t.__class__.__name__ in r, 'Does not contain its own name'
+    assert '1' in r, 'Value not in output'
+    assert '2' in r, 'Value not in output'
+    assert '3' in r, 'Value not in output'
+    assert '4' in r, 'Value not in output'
+                   
